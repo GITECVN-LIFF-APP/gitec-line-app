7c36f41a0d0bb4eebd70349e2f05c9741f93aa3f
thong.le <thong.le@asnet.com.vn>
2022-11-12 01:10:00 +0700

    feat: implement MemberRegister component

---
+++ client/src/components/MemberRegister/index.stories.tsx
@@ -0,0 +1,9 @@
+import { Story } from '@storybook/react';
+import MemberRegister from '@components/MemberRegister';
+
+export default {
+  title: 'MemberRegister',
+  component: MemberRegister,
+};
+
+export const RegisterSample: Story = () => <MemberRegister />;
