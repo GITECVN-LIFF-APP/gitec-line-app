6ca6956821433f54d406646aac32558c03c8c8a6
Son Le Tran <letranson1992@gmail.com>
2022-12-01 22:16:19 +0700

    Update: Fix bug UI

--- client/src/components/CardHorizontal/index.tsx
+++ client/src/components/CardHorizontal/index.tsx
@@ -4,24 +4,21 @@ export type CardHorizontalProps = {
   description: string
 }
 
-
-const CardHorizontal = ({image, title, description}:CardHorizontalProps) => {
+const CardHorizontal = ({ image, title, description }: CardHorizontalProps) => {
   return (
-    <>
-      <div className='homepagecard'>
-        <div className='content'>
-          <div className='card'>
-            <div className='firstinfo'>
-              <img src={image} />
-              <div className='profileinfo'>
-                <p className='fw-bold mb-4'>{title}</p>
-                <p className='bio'>{description}</p>
-              </div>
+    <div className='homepagecard'>
+      <div className='content'>
+        <div className='card'>
+          <div className='firstinfo'>
+            <img src={image} />
+            <div className='profileinfo'>
+              <p className='fw-bold mb-4'>{title}</p>
+              <p className='bio'>{description}</p>
             </div>
           </div>
         </div>
       </div>
-    </>
+    </div>
   )
 }
 export default CardHorizontal
