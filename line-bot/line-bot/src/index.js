af17eb3fa574e0bc0f5c3914230d34ab37003213
Son Le Tran <letranson1992@gmail.com>
2023-01-09 06:37:44 +0700

    Update: Fix bot

--- line-bot/line-bot/src/index.js
+++ line-bot/line-bot/src/index.js
@@ -1,3 +1,24 @@
-module.exports = async function App(context) {
-  await context.sendText('Welcome to Bottender');
-};
+function initializeLiff(myLiffId) {
+  liff
+    .init({
+      liffId: myLiffId,
+    })
+    .then(() => {
+      alert('LIFF init success!');
+    })
+    .catch((err) => {
+      alert(`error: ${JSON.stringify(err)}`);
+    });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  fetch(`/send-id`)
+    .then((reqResponse) => reqResponse.json())
+    .then((jsonResponse) => {
+      let myLiffId = jsonResponse.id;
+      initializeLiff(myLiffId);
+    })
+    .catch((err) => {
+      alert(`error: ${JSON.stringify(err)}`);
+    });
+});
